66cd037b152570cffda118ba97c15e4ae02b94b2
Alex <alex_step04@mail.ru>
2024-09-09 01:04:01 +0300

    attempt 1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,61 @@
 package main
 
-func main() {}
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"io"
+	"net/http"
+)
+
+type ShortURL struct {
+    URL       string
+    shortURL  string
+}
+
+var urlMap = make(map[string]ShortURL)
+
+func shortingURL(res http.ResponseWriter, req *http.Request) {    
+    if req.Method == http.MethodPost {
+        body, _ := io.ReadAll(req.Body)
+        
+        url := string(body)
+
+        hash := sha256.New()
+        hashString := base64.StdEncoding.EncodeToString(hash.Sum([]byte(url)))
+        hashShortString := string([]rune(hashString)[len(hashString)-10:len(hashString)-2])
+        urlMap[hashShortString] = ShortURL{
+            URL: url,
+            shortURL: "http://localhost:8080/"+hashShortString,
+        }
+        
+        res.Header().Set("content-type", "text/plain")
+        res.WriteHeader(http.StatusOK)
+
+        res.Write([]byte(urlMap[hashShortString].shortURL))
+    } else { 
+        res.WriteHeader(http.StatusBadRequest)
+    }
+}
+
+func getUrl(res http.ResponseWriter, req *http.Request) { 
+    if req.Method == http.MethodGet {
+        shorturl := urlMap[req.PathValue("shorturl")].URL
+        res.Header().Add("location", shorturl)
+        res.WriteHeader(http.StatusTemporaryRedirect)
+    } else { 
+        res.WriteHeader(http.StatusBadRequest)
+    }
+
+}
+
+func main() {
+
+    mux := http.NewServeMux()
+    mux.HandleFunc(`/`, shortingURL)
+    mux.HandleFunc(`/{shorturl}`, getUrl)
+
+    err := http.ListenAndServe(`:8080`, mux)
+    if err != nil {
+        panic(err)
+    }
+} 
