6b4c5b9114d31a65dc493e5fdd9a9b469b521bf9
Alex <alex_step04@mail.ru>
2024-09-09 17:33:03 +0300

    attempt 10

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -36,7 +36,7 @@ func Test_getUrl(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			getUrl(tt.args.res, tt.args.req)
+			getURL(tt.args.res, tt.args.req)
 		})
 	}
 }
