6ab29fb45a26b9595cfdb6e735f0309a0b674353
Alex <alex_step04@mail.ru>
2024-09-09 16:39:47 +0300

    attempt 5

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,6 +5,9 @@ import (
 	"encoding/base64"
 	"io"
 	"net/http"
+
+    "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type ShortURL struct {
@@ -14,47 +17,39 @@ type ShortURL struct {
 
 var urlMap = make(map[string]ShortURL)
 
-func shortingURL(res http.ResponseWriter, req *http.Request) {    
-    if req.Method == http.MethodPost {
-        body, _ := io.ReadAll(req.Body)
-        
-        url := string(body)
+func shortingURL(res http.ResponseWriter, req *http.Request) {
+    body, _ := io.ReadAll(req.Body)
+    url := string(body)
+    hash := sha256.New()
+    hashString := base64.StdEncoding.EncodeToString(hash.Sum([]byte(url)))
+    hashShortString := string([]rune(hashString)[len(hashString)-10:len(hashString)-2])
 
-        hash := sha256.New()
-        hashString := base64.StdEncoding.EncodeToString(hash.Sum([]byte(url)))
-        hashShortString := string([]rune(hashString)[len(hashString)-10:len(hashString)-2])
-        urlMap[hashShortString] = ShortURL{
-            URL: url,
-            shortURL: "http://localhost:8080/"+hashShortString,
-        }
-        
-        res.Header().Set("content-type", "text/plain")
-        res.WriteHeader(http.StatusCreated)
-
-        res.Write([]byte(urlMap[hashShortString].shortURL))
-    } else { 
-        res.WriteHeader(http.StatusBadRequest)
+    urlMap[hashShortString] = ShortURL{
+        URL: url,
+        shortURL: "http://localhost:8080/"+hashShortString,
     }
+    
+    res.Header().Set("content-type", "text/plain")
+    res.WriteHeader(http.StatusCreated)
+    res.Write([]byte(urlMap[hashShortString].shortURL))
 }
 
 func getUrl(res http.ResponseWriter, req *http.Request) { 
-    if req.Method == http.MethodGet {
-        shorturl := urlMap[req.PathValue("shorturl")].URL
-        res.Header().Add("location", shorturl)
-        res.WriteHeader(http.StatusTemporaryRedirect)
-    } else { 
-        res.WriteHeader(http.StatusBadRequest)
-    }
-
+    shorturl := urlMap[chi.URLParam(req, "shorturl")].URL
+    res.Header().Add("location", shorturl)
+    res.WriteHeader(http.StatusTemporaryRedirect)
 }
 
 func main() {
+    r := chi.NewRouter()
+    r.Use(middleware.RequestID)
+    r.Use(middleware.RealIP)
+    r.Use(middleware.Logger)
+    r.Use(middleware.Recoverer)
+    r.Post("/", shortingURL)
+    r.Get("/{shorturl}", getUrl)
 
-    mux := http.NewServeMux()
-    mux.HandleFunc(`/`, shortingURL)
-    mux.HandleFunc(`/{shorturl}`, getUrl)
-
-    err := http.ListenAndServe(`:8080`, mux)
+    err := http.ListenAndServe(`:8080`, r)
     if err != nil {
         panic(err)
     }
